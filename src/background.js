9c8785af4a4ab14c43bc297faafd879cc984c6b6
Akshay Kashyap <akshay.kashyap95@gmail.com>
2024-01-10 22:55:31 -0800

    rm contextmenu

--- src/background.js
+++ src/background.js
@@ -30,7 +30,7 @@ const classify = async (text) => {
     let model = await PipelineSingleton.getInstance((data) => {
         // You can track the progress of the pipeline creation here.
         // e.g., you can send `data` back to the UI to indicate a progress bar
-        console.log('progress', data);
+        // console.log('progress', data);
     });
 
     // Actually run the model on the input text
@@ -43,37 +43,10 @@ const classify = async (text) => {
 // Add a listener to create the initial context menu items,
 // context menu items only need to be created at runtime.onInstalled
 chrome.runtime.onInstalled.addListener(function () {
-    // Register a context menu item that will only show up for selection text.
-    // chrome.contextMenus.create({
-    //     id: 'classify-selection',
-    //     title: 'Classify "%s"',
-    //     contexts: ['selection'],
-    // });
-
 	chrome.alarms.create('librarian-indexer', {
 		periodInMinutes: 15
 	});
 });
-
-// Perform inference when the user clicks a context menu
-// chrome.contextMenus.onClicked.addListener(async (info, tab) => {
-//     // Ignore context menu clicks that are not for classifications (or when there is no input)
-//     if (info.menuItemId !== 'classify-selection' || !info.selectionText) return;
-
-//     // Perform classification on the selected text
-//     let result = await classify(info.selectionText);
-
-//     // Do something with the result
-//     chrome.scripting.executeScript({
-//         target: { tabId: tab.id },    // Run in the tab that the user clicked in
-//         args: [result],               // The arguments to pass to the function
-//         function: (result) => {       // The function to run
-//             // NOTE: This function is run in the context of the web page, meaning that `document` is available.
-//             console.log('result', result)
-//             console.log('document', document)
-//         },
-//     });
-// });
 
 // Indexer scheduling
 chrome.alarms.onAlarm.addListener(async (alarm) => {
